21ae6ed3099f00473bb9271729007b21bc32095b
AlexASR <100frand@gmail.com>
2017-10-10 17:39:26 +0300

    tets1

---
+++ Rocket1/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Rocket1
+//
+//  Created by Alex on 09.10.17.
+//  Copyright © 2017 Alex. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var launchModeView: UIView!
+    @IBOutlet weak var rocketImageView: UIImageView!
+    @IBOutlet weak var onLabel: UILabel!
+    @IBOutlet weak var launchLabel: UILabel!
+    
+    var player: AVAudioPlayer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let path = Bundle.main.path(forResource: "rocket_launch", ofType: "mp3")!
+        let url = URL(fileURLWithPath: path)
+        player = try? AVAudioPlayer(contentsOf: url)
+        player?.prepareToPlay()
+    }
+    
+    @IBAction func launchPress(_ sender: Any) {
+        launchModeView.isHidden = false
+        player?.play()
+        UIView.animate(withDuration: 3.0, animations: {
+            var rocketFrame = self.rocketImageView.frame
+            rocketFrame.origin.y = 70
+            self.rocketImageView.frame = rocketFrame
+        }) { _ in
+            self.launchLabel.isHidden = false
+            self.onLabel.isHidden = false
+        }
+    }
+}
+
